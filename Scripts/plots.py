3f136da2ac540e5db8c1307e73a4c3b9f10ab70b
Sushil Kumar <adithyaiyer1999@gmail.com>
2020-08-16 23:15:56 +0530

    Events, convexity and distributions

--- Scripts/plots.py
+++ Scripts/plots.py
@@ -29,8 +29,8 @@ def plotFromDataOnlyY(y,xlabel='xlabel',ylabel='ylabel', title='title'):
     plt.rc('font', family='serif')
     plt.rcParams["axes.edgecolor"] = "black"
     plt.rcParams["axes.linewidth"] = 1
-    plt.rc('xtick', labelsize='x-small')
-    plt.rc('ytick', labelsize='x-small')
+    plt.rc('xtick', labelsize='x-large')
+    plt.rc('ytick', labelsize='x-large')
     fig = plt.figure(figsize=(8, 6))
     ax = fig.add_subplot(1, 1, 1)
     ax.set_xlabel(xlabel)
